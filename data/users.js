82a7d303e2c52df37df4b0aaf66d9a9afefe4085
Alhassan-Nazifa <naalhassan174@gmail.com>
2022-04-07 08:22:37 -0700

    first commit

---
+++ data/users.js
@@ -0,0 +1,8 @@
+const user=[
+    {id:"1", name:"Nazifa", age:"28"},
+    {id:"2", name:"Sadika", age:"22"},
+    {id:"3", name:"Korkor", age:"21"},
+    {id:"4", name:"Hafiza", age:"26"},
+    {id:"5", name:"Aminat", age:"22"},
+]
+module.exports=user

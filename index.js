47bd1146d76a10e17dcc753323d8cd465b3ad210
Alhassan-Nazifa <naalhassan174@gmail.com>
2022-04-11 04:12:44 -0700

    added new file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express =require("express")
 const usersRoute =require("./routes/usersRoutes")
+const path = require("path")
 
 const app = express()
 
@@ -10,11 +11,17 @@ app.use(usersRoute)
 
 //home route
 app.get("/",(req, res)=>{
-    res.status(200).send("<h1>WELCOME TO THE USERS DATABASE</h1>");
+    res.sendFile(path.join(__dirname + "/pages/index.html"))
+
 
 })
 
-const PORT =4000
+app.get("/*", (req, res)=>{
+    res.status(400).sendFile(path.join(__dirname + "/pages/404.html"))
+
+})
+
+const PORT =4000;
 
 app.listen(PORT, ()=>{
     console.log("SERVER IS UP")
